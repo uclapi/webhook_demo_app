50bba3a44c75781b2f582f1355d720e0701badd0
Wilhelm Klopp <wil.klopp@gmail.com>
2017-08-20 16:55:24 +0100

    Fix STATIC_ROOT

--- webhook_demo_app/settings.py
+++ webhook_demo_app/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Django Channels
 CHANNEL_LAYERS = {
